57b5a7246c90c9e7a730ec58a77b42418863ef55
rashid <rashid@techkit.in>
2023-05-15 18:12:49 +0530

    new

--- lib/message_model.dart
+++ lib/message_model.dart
@@ -1,29 +1,26 @@
-// To parse this JSON data, do
-//
-//     final messagemodel = messagemodelFromJson(jsonString);
-
 import 'dart:convert';
 
-List<Messagemodel> messagemodelFromJson(String str) => List<Messagemodel>.from(json.decode(str).map((x) => Messagemodel.fromJson(x)));
-
-String messagemodelToJson(List<Messagemodel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+List<Messagemodel> messagemodelFromJson(String str) => List<Messagemodel>.from(
+    json.decode(str).map((x) => Messagemodel.fromJson(x)));
+String messagemodelToJson(List<Messagemodel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Messagemodel {
-    String senderId;
-    String text;
+  String senderId;
+  String text;
 
-    Messagemodel({
-        required this.senderId,
-        required this.text,
-    });
+  Messagemodel({
+    required this.senderId,
+    required this.text,
+  });
 
-    factory Messagemodel.fromJson(Map<String, dynamic> json) => Messagemodel(
+  factory Messagemodel.fromJson(Map<String, dynamic> json) => Messagemodel(
         senderId: json["senderId"],
         text: json["text"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "senderId": senderId,
         "text": text,
-    };
+      };
 }
